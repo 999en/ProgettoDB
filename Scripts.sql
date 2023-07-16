a829700caf3c5e418dc50b78f751c7c148e3ceaf
999en <125801405+999en@users.noreply.github.com>
2023-07-16 19:22:07 +0200

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -461,32 +461,45 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
 	CREATE TRIGGER inserimento_Galleria
 	BEFORE INSERT ON Contenuto
 	FOR EACH ROW
 	EXECUTE FUNCTION controllo_autore();
 
+---------------------------------------------------------------------------------------------------------------------------------------------
+--Trigger che elimina frame nei video dopo che una foto è stata resa privata
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+CREATE OR REPLACE FUNCTION rimuovi_foto_privata() RETURNS TRIGGER AS $$
+BEGIN
+	DELETE FROM frame WHERE id_foto = OLD.id_foto;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER rimuovi_foto_privata_dopo_update
+AFTER UPDATE OF condivisa ON fotografia
+FOR EACH ROW WHEN (NEW.condivisa = false)
+EXECUTE PROCEDURE rimuovi_foto_privata();
 
 ---------------------------------------------------------------------------------------------------------------------------------------------
---Trigger che impedisce ad un'utente che di utilizzare foto private se non ne è l'autore nei video
+--Trigger che impedisce ad un'utente che di utilizzare foto private nei video
 ---------------------------------------------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION controllo_autore_video() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION controllo_privacy_video() RETURNS TRIGGER AS $$
 DECLARE
 proprietario_foto VARCHAR;
 proprietario_video VARCHAR;
 BEGIN
-	SELECT username_autore INTO proprietario_foto FROM fotografia WHERE id_foto=NEW.id_foto;
-	SELECT autore INTO proprietario_video FROM video WHERE id_video=NEW.id_video;
-    IF NOT EXISTS (SELECT * FROM fotografia WHERE id_foto = NEW.id_foto AND ((condivisa = true) OR (proprietario_foto = proprietario_video))) THEN
-        RAISE EXCEPTION 'Non sei autorizzato ad utilizzare questa foto';
-    END IF;
-    RETURN NEW;
+  IF (SELECT condivisa FROM fotografia WHERE NEW.id_foto = fotografia.id_foto) = false THEN
+    RAISE EXCEPTION 'Non è possibile utilizzare foto private per video.';
+  END IF;
+  RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 	CREATE TRIGGER inserimento_frame
 	BEFORE INSERT ON frame
 	FOR EACH ROW
-	EXECUTE FUNCTION controllo_autore_video();
+	EXECUTE FUNCTION controllo_privacy_video();
